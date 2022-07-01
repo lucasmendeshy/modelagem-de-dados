2b8ac068a6e0ed9db23492053936b0b6e732d5ef
Lucas Mendes <lucasaraujo12345@outlook.com.br>
2022-07-01 10:07:53 -0300

    Update cinema2.sql

--- cinema2.sql
+++ cinema2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 27-Abr-2022 às 16:57
+-- Tempo de geração: 01-Jul-2022 às 15:07
 -- Versão do servidor: 10.4.22-MariaDB
 -- versão do PHP: 8.0.13
 
@@ -34,6 +34,16 @@ CREATE TABLE `filmes` (
   `genero_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Extraindo dados da tabela `filmes`
+--
+
+INSERT INTO `filmes` (`id`, `titulo`, `lancamento`, `genero_id`) VALUES
+(1, 'Velozes e Furiosos 1', 2001, 1),
+(2, 'O poço', 2019, 2),
+(3, 'Avatar', 2009, 3),
+(4, 'As Branquelas', 2004, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -44,6 +54,17 @@ CREATE TABLE `genero` (
   `id` int(11) NOT NULL,
   `genero` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `genero`
+--
+
+INSERT INTO `genero` (`id`, `genero`) VALUES
+(1, 'ação'),
+(2, 'terror'),
+(3, 'aventura'),
+(4, 'comédia'),
+(5, 'Romance');
 
 --
 -- Índices para tabelas despejadas
@@ -70,13 +91,13 @@ ALTER TABLE `genero`
 -- AUTO_INCREMENT de tabela `filmes`
 --
 ALTER TABLE `filmes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de tabela `genero`
 --
 ALTER TABLE `genero`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restrições para despejos de tabelas
